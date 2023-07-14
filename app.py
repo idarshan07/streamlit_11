4cdc75e1b63260a38e86c44e6b849656bedd10d8
vaibhavsheth15 <vaibhavsheth15@gmail.com>
2023-07-13 21:51:14 -0400

    first commit

--- app.py
+++ app.py
@@ -10,3 +10,14 @@ import os
 # Load the pre-trained model
 model = models.resnet18(pretrained=True)
 model.eval()
+
+def load_labels():
+    labels_path = 'https://raw.githubusercontent.com/pytorch/hub/master/imagenet_classes.txt'
+    labels_file = os.path.basename(labels_path)
+    if not os.path.exists(labels_file):
+        wget.download(labels_path)
+    with open(labels_file, "r") as f:
+        categories = [s.strip() for s in f.readlines()]
+        return categories
+    
+    
